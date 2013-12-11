cec7060ebaecc4efcac950962a9fef4e6885d9bd
CheesecakeShop <scooter_boy_22@hotmail.com>
2013-12-12 01:43:20 +1000

    Update mybeacon.lua

--- mybeacon.lua
+++ mybeacon.lua
@@ -10,7 +10,7 @@ function initializeObject()
 end
 
 function onInteraction(args)
-    world.playerQuery(object.position, self.noticePlayersRadius, { inSightOf = self.id() })
+    world.playerQuery(object.position, self.noticePlayersRadius, { inSightOf = entity.id() })
     --world.spawnMonster("serpentdroid", object.toAbsolutePosition({ 0.0, 5.0 }), { level = 1 })
     return { "ShowPopup", { message = "Testing!" } }
 end
