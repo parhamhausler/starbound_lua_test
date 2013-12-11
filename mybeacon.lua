cbb1cf9d3cbae65c16967c48f27a6b1540dc891f
CheesecakeShop <scooter_boy_22@hotmail.com>
2013-12-12 00:39:26 +1000

    Update mybeacon.lua

--- mybeacon.lua
+++ mybeacon.lua
@@ -2,6 +2,16 @@ function init(args)
 	object.setInteractive(true)
 end
 
+function main()
+    if self.initialized == nil then
+        initializeObject();
+        self.initialized = true;
+    end
+end
+
+function initializeObject()
+    object.setInteractive(true);
+end
 
 function onInteraction(args)
 	return { "ShowPopup", { message = "Testing!" } }
