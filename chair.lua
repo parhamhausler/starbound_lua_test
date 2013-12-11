62e91b6ea44ac90c58b19797d9038c6cff9f76be
parhamhausler <parhamhausler@yahoo.com.au>
2013-12-12 00:22:16 +1100

    Create chair.lua
    test

---
+++ chair.lua
@@ -0,0 +1,10 @@
+function main()
+    if self.initialized == nil then
+        initializeObject();
+        self.initialized = true;
+    end
+end
+
+function onInteraction(args)
+  object.playSound("\sfx\gglaugh.wav")
+end
