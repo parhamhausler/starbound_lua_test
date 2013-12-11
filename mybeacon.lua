83b00a2753aa5c98252fdeebb9c15a5b41cb2541
CheesecakeShop <scooter_boy_22@hotmail.com>
2013-12-12 01:36:55 +1000

    Update mybeacon.lua

--- mybeacon.lua
+++ mybeacon.lua
@@ -10,6 +10,8 @@ function initializeObject()
 end
 
 function onInteraction(args)
+    plog = world.playerQuery(args)
+    world.logInfo("plog")
     object.playSound("closeSounds")
     world.spawnMonster("serpentdroid", object.toAbsolutePosition({ 0.0, 5.0 }), { level = 1 })
     return { "ShowPopup", { message = "Testing!" } }
