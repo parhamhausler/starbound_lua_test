7b79f4a01a8f4f975d22b4d22768672de337f33c
CheesecakeShop <scooter_boy_22@hotmail.com>
2013-12-11 23:52:40 +1000

    Update chair.lua

--- chair.lua
+++ chair.lua
@@ -1,14 +1,14 @@
 function main()
     if self.initialized == nil then
-        initializeObject()
-        self.initialized = true
+        initializeObject();
+        self.initialized = true;
     end
 end
 
 function initializeObject()
-    object.setInteractive(true)
+    object.setInteractive(true);
 end
 
 function onInteraction(args)
-    object.playSound("\sfx\gglaugh.wav")
+    object.playSound("\sfx\gglaugh.wav");
 end
